324643b762c4f1a543e341a56acac218ecb31a99
PtrO <34420738+ptrooms@users.noreply.github.com>
2025-07-16 01:08:10 +0200

    v55b having 15 delays in unused code, stabilizes runtime

--- P1Meter/P1Meter.ino
+++ P1Meter/P1Meter.ino
@@ -5190,7 +5190,7 @@ void command_testH3(){    // publish mqtt records in TEST_MODE
 }
 
 void command_testH4(){    // code to maken things stable teststable
-                          // we remoived some unused protection arrays, improved ISR-time, 
+                    // we remoived some unused protection arrays, improved ISR-time, 
                     delay(0);     // v55b , stable1
                     delay(0);     // v55b , stable0
                     delay(0);     // v55b , stable4
@@ -5200,10 +5200,15 @@ void command_testH4(){    // code to maken things stable teststable
                     delay(0);     // v55b , stable0
                     delay(0);     // v55b , stable2
                     delay(0);     // v55b , stable1
-                    delay(0);     // v55b , stable10 <-- very very good at this state
-                    // v55b continue to test here if things become unstable ......
-                    delay(0);     // v55b , 
-                    // // delay(0);     // stable but did caused a runtime crash 
+                    // ------------------------------------------------------------
+                    delay(0);     // v55b , stable8 <-- very very good
+                    // v55b continue to test here if things become better or worse
+                    delay(0);     // v55b , stable6 
+                    delay(0);     // v55b , stable7
+                    delay(0);     // v55b , stable10 <-- even better P1>95% RX2=50%
+                    delay(0);     // v55b , stable9  <-- even better P1>95% RX2=70%
+                    // ------------------------------------------------------------
+
 
 }
 
