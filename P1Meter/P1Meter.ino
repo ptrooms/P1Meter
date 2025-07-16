249423f2617e48dbb4c8525a8f4468cdb09e9129
PtrO <34420738+ptrooms@users.noreply.github.com>
2025-07-16 18:22:33 +0200

    v55b commented this is working great

--- P1Meter/P1Meter.ino
+++ P1Meter/P1Meter.ino
@@ -40,6 +40,7 @@
 */
 
 /* change history
+  v55b = perfectly tuned
   v55a - debug to checkout code differences for: teststable
     - introducing DUP_MODE to identically replicate TESYT/PROD_MODE with
          reference DUP_MODE with TEST_MODE ip 185 = ip 185 with prefix e1 and version 3xxxx
