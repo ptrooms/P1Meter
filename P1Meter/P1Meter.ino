cafdf4f10d6b94a7cb21123a7ff35ce01f0ea0c4
PtrO <34420738+ptrooms@users.noreply.github.com>
2025-07-16 18:24:03 +0200

    v55d commented its ok

--- P1Meter/P1Meter.ino
+++ P1Meter/P1Meter.ino
@@ -40,6 +40,9 @@
 */
 
 /* change history
+  v55d - works good, based on v55c but added some serial.debugs, a bit worse than v55b
+  v55c - woes completely on serial
+  v55b = perfectly tuned
   v55a - debug to checkout code differences for: teststable
     - introducing DUP_MODE to identically replicate TESYT/PROD_MODE with
          reference DUP_MODE with TEST_MODE ip 185 = ip 185 with prefix e1 and version 3xxxx
